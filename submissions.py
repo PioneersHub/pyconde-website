432cbba9a031c5c8640498a8c1500e162975a1dd
Ömür Özkir <oemuer.oezkir@gmail.com>
2025-01-20 09:28:13 +0100

    adds a little script to get the talks from pretalks

---
+++ submissions.py
@@ -0,0 +1,18 @@
+from pytanis import PretalxClient
+import json
+
+def normalize_submission(submission):
+    submission = submission.model_dump()
+    submission['state'] = submission['state'].value
+    return submission
+
+def main():
+    event_name = "pyconde-pydata-2025"
+    client = PretalxClient()
+    _, subs = client.submissions(event_name)
+    subs = list(subs)
+    with open("databags/submissions.json", "w") as f:
+        f.write(json.dumps([normalize_submission(sub) for sub in subs], default=str))
+
+if __name__ == "__main__":
+    main()
