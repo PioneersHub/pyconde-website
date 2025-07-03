d695d47d51243422a80db173e4d4eaff690d0b35
Ömür Özkir <oemuer.oezkir@gmail.com>
2025-07-03 12:35:54 +0200

    updates image generation and tweaks the image reel styling on the landing page

--- utils/landing_page.py
+++ utils/landing_page.py
@@ -2,7 +2,7 @@ import json
 from os import listdir
 
 ASSET_FOLDERS = {
-    "conference_images": "assets/static/landing-page/conference/",
+    "venue_images": "assets/static/landing-page/venue/",
     "content_images": "assets/static/landing-page/content/",
     "community_images": "assets/static/landing-page/community/",
     "speaker_images": "assets/static/landing-page/speakers/",
@@ -17,7 +17,9 @@ def assets_to_json_file(assets: dict):
 
 
 def files_to_relative_paths(relative_path: str) -> list[str]:
-    return [relative_path.replace("assets", "") + p for p in listdir(relative_path)]
+    return [
+        relative_path.replace("assets", "") + p for p in sorted(listdir(relative_path))
+    ]
 
 
 def main():
